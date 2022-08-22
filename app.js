83dc3c624b5d736971cdbc14a77dbe158c670843
allenaw12 <102706128+allenaw12@users.noreply.github.com>
2022-08-22 13:10:51 -0500

    added routes for auth and logins

--- app.js
+++ app.js
@@ -4,10 +4,14 @@ const dotenv = require('dotenv')
 const morgan = require('morgan')
 const exphbs = require('express-handlebars')
 const passport = require('passport')
+const session = require('express-session')
 const connectDB = require('./config/db')
 
 //load config
 dotenv.config({path: './config/config.env'})
+
+//passport config
+require('./config/passport')(passport)
 
 connectDB()
 
@@ -22,11 +26,24 @@ if(process.env.NODE_ENV === 'development') {
 app.engine('.hbs', exphbs.engine({defaultLayout: 'main', extname: '.hbs'}))
 app.set('view engine', '.hbs')
 
+//sessions (must be above passport, since it uses this middleware)
+app.use(session({
+    secret: 'dinosaur disco',
+    resave: false,
+    saveUninitialized: false,
+    
+}))
+
+//passport middleware
+app.use(passport.initialize())
+app.use(passport.session())
+
 //static folder
 app.use(express.static(path.join(__dirname, 'public')))
 
 //routes
 app.use('/', require('./routes/index'))
+app.use('/auth', require('./routes/auth'))
 
 const PORT = process.env.PORT || 8811
 
